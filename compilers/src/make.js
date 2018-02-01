75397a7a70bbcd9b74244b28a1ac452308707d0b
Kevin <18480985+kevzdev@users.noreply.github.com>
2018-02-01 11:46:20 +0100

    Update make.js
    Fixes `[DEP0013] DeprecationWarning: Calling an asynchronous function without callback is deprecated.` in Windows.

--- compilers/src/make.js
+++ compilers/src/make.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-const domains = fs.readFileSync('../../domains', 'utf8').split('\n')
+const domains = fs.readFileSync('domains', 'utf8').split('\n')
 
-fs.appendFile('hostfile', '##COINHIVE DOMAINS\n')
+fs.appendFile('hostfile', '##COINHIVE DOMAINS\n', ()=>{})
 domains.forEach((domain) => {
-  fs.appendFile('../../hostfile', '0.0.0.0 ' + domain + '\n')
+  fs.appendFile('hostfile', '0.0.0.0 ' + domain + '\n', ()=>{})
 })
-fs.appendFile('hostfile', '##END COINHIVE\n')
+fs.appendFile('hostfile', '##END COINHIVE\n', ()=>{})
