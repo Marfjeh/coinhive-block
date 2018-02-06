72e34eb308d987bee6787e42aa2f6e9321eabef6
Kevin <18480985+kevzdev@users.noreply.github.com>
2018-02-06 18:58:56 +0100

    Update make.js

--- compilers/src/make.js
+++ compilers/src/make.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 
 const domains = fs.readFileSync('../../domains', 'utf8').split('\n')
 
-fs.appendFile('../../hostfile', '##COINHIVE DOMAINS\n', ()=>{})
+fs.appendFile('../../hostfile', '##COINHIVE DOMAINS\n', function () {})
 domains.forEach((domain) => {
-  fs.appendFile('../../hostfile', '0.0.0.0 ' + domain + '\n', ()=>{})
+  fs.appendFile('../../hostfile', '0.0.0.0 ' + domain + '\n', function () {})
 })
-fs.appendFile(../../'hostfile', '##END COINHIVE\n', ()=>{})
+fs.appendFile('../../hostfile', '##END COINHIVE\n', function () {})
